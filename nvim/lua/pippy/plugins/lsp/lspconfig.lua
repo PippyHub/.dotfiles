51d9eab628e7b9fbf454a327129e6b77e18d985d
PippyHub <piper.innshall@gmail.com>
2024-05-05 20:11:59 +1200

    fixed lsp issues

--- nvim/lua/pippy/plugins/lsp/lspconfig.lua
+++ nvim/lua/pippy/plugins/lsp/lspconfig.lua
@@ -1,5 +1,5 @@
 return {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
@@ -8,9 +8,8 @@ return {
         'hrsh7th/cmp-nvim-lsp',
         'L3MON4D3/LuaSnip',
     },
-
-    config = function() 
-        local lspconfig = require('lspconfig')
+    config = function()
+        local lsp = require('lspconfig')
         local mason = require('mason')
         local masonlsp = require('mason-lspconfig')
         local masontool = require('mason-tool-installer')
@@ -19,30 +18,36 @@ return {
         local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         local default_setup = function(server)
-            lspconfig[server].setup({
-                capabilities = lsp_capabilities,
-            })
+            if server ~= 'jdtls' then
+                lsp[server].setup({
+                    capabilities = lsp_capabilities,
+                })
+            end
         end
 
         mason.setup({})
         masonlsp.setup({
+            ensure_installed = {},
             handlers = {
                 default_setup,
             },
         })
         masontool.setup({})
 
-
         cmp.setup({
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
             sources = {
                 {name = 'nvim_lsp'},
             },
             mapping = cmp.mapping.preset.insert({
-                -- Enter key confirms completion item
-                ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-                -- Ctrl + space triggers completion menu
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
             }),
             snippet = {
                 expand = function(args)
