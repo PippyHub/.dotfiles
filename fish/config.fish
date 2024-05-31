2d4198047bb50ba5a84580a3c76c7c3cf97c8220
PippyHub <piper.innshall@gmail.com>
2024-05-31 12:19:53 +1200

    add root to git

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias fdir "cd (fzf --preview='cat {}' --walker=dir)"
+alias fzf "fzf --preview='cat {}'"
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
