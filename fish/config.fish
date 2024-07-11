19a421ac708df26aef9181b2b00a5782076e71c3
PippyHub <piper.innshall@gmail.com>
2024-07-11 15:01:46 +1200

    Rice-1 commit

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,5 @@ alias fdir "cd (fzf --preview='cat {}' --walker=dir)"
 alias fzf "fzf --preview='cat {}'"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+set -x PATH /Users/piperinnshall/Developer/src/godot/bin $PATH
+set -x PATH /Users/piperinnshall/Developer/src/aseprite/build/bin $PATH
