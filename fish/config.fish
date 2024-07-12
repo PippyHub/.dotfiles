84634820e247f784601bdf956371771e27c92198
PippyHub <piper.innshall@gmail.com>
2024-07-12 16:21:55 +1200

    Rice-1 final commit

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias fdir "cd (fzf --preview='cat {}' --walker=dir)"
 alias fzf "fzf --preview='cat {}'"
+alias fdir "cd (fzf --preview='cat {}' --walker=dir)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 set -x PATH /Users/piperinnshall/Developer/src/godot/bin $PATH
 set -x PATH /Users/piperinnshall/Developer/src/aseprite/build/bin $PATH
+set -x PATH /Users/piperinnshall/Developer/src/sketchybar/bin $PATH
