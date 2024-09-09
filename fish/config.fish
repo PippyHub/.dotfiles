d34a7a2ddb15a10c7f964dedeb2ad14f0ddc66ae
piperinnshall <piper.innshall@gmail.com>
2024-09-09 16:45:06 +1200

    chore(fish): add gpg key to path

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,12 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 alias fzf "fzf --preview='cat {}'"
 alias fdir "cd (fzf --preview='cat {}' --walker=dir)"
 
-set -x PATH /Users/piperinnshall/Developer/scripts/fish $PATH
-set -x PATH /Users/piperinnshall/Developer/scripts/bash $PATH
+set -x PATH /Users/piperinnshall/Developer/scripts/* $PATH
 
 set -x PATH /Users/piperinnshall/Developer/src/godot/bin $PATH
 set -x PATH /Users/piperinnshall/Developer/src/aseprite/build/bin $PATH
+
+set -x GPG_TTY (tty)
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
